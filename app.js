f7f3d836ae5c042ba57818e65a738efde7c500b8
Diego Oliveira <diego.business77@gmail.com>
2025-02-23 17:05:05 -0300

    teste de exclusão de commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
+//Comentário para teste de exclusão de commit
+
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
